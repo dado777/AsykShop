a0673e6180e9453e06b42d16a03101e31823b0ac
Дәурен Әбілхан <abilkhan_d@akt.nis.edu.kz>
2021-12-22 11:21:28 +0500

    Added: HomeController Fixed: angle bracket in index page

---
+++ AsykShop/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using AsykShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsykShop.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Asyk> FavouriteAsyktar { get; set; }
+    }
+}
