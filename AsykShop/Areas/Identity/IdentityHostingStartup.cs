a2dbec8fb04ef59f77d3c76d7c3410f6e17299ab
Дәурен Әбілхан <abilkhan_d@akt.nis.edu.kz>
2022-09-19 16:01:38 +0500

    Added redirection to admin page

---
+++ AsykShop/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using AsykShop.Areas.Identity.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AsykShop.Areas.Identity.IdentityHostingStartup))]
+namespace AsykShop.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AsykShopAccountContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AsykShopAccountContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<AsykShopAccountContext>();
+            });
+        }
+    }
+}
